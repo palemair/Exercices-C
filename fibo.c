96b7abe395820d3135acfdc8947164120c5f97d3
palemair <palemair@gmail.com>
2024-09-09 21:52:07 +0200

    fibo

--- fibo.c
+++ fibo.c
@@ -3,49 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-void afft(int nbt, int tab[][nbt]);
-
-void afft(int nbt, int tab[][nbt])
+int main (int argc, char** argv)
 {
-  for(int u = 0; u<nbt; u++)
-  {
-      for(int y = 0; y<nbt; y++) 
-          if(tab[u][y] == 0) putchar(' ');
-          else printf("%4d ",tab[u][y]);
-      putchar('\n');
-  }
 
-}
-
-int main(int argc, char **argv) {
-  
-    char *nbl; 
+    char* nbl;
     int nb;
 
-
-    if (argc ==2) 
+    if (argc == 2)
     {
-        nbl = strdupa(argv[1]);
-        sscanf(nbl,"%d",&nb) ; 
+        nbl = strdupa (argv[1]);
+        sscanf (nbl, "%d", &nb);
     }
     else
     {
         nb = 10;
     }
 
-int som = 0;
+    int som = 0;
+    int u, v;
+    u = 0;
+    v = 1;
 
-int u,v;
-u=v=1;
+    while (nb-- > 0)
+    {
+        som = u + v;
+        printf ("%d + %d = %d\n", u, v, som);
+        u = v;
+        v = som;
+    }
 
-while(nb>0)
-{
-  som = u + v;
-  printf("%d + %d = %d\n",u,v,som);
-  v=u;
-  u=som;
-  nb--;
-}
-
-  return 0;
+    return 0;
 }
